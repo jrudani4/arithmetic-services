f94e2c5fad82ed661ccb232ffbc74aa3d8de0283
Jay Rudani <jrudani4@gmail.com>
2023-02-08 17:52:57 +0530

    test updated to parameterized test

--- multiplication-service/src/test/java/com/arithmeticservices/multiplicationservice/MultiplicationControllerTest.java
+++ multiplication-service/src/test/java/com/arithmeticservices/multiplicationservice/MultiplicationControllerTest.java
@@ -2,22 +2,28 @@ package com.arithmeticservices.multiplicationservice;
 
 import com.arithmeticservices.multiplicationservice.controller.MultiplicationController;
 import com.arithmeticservices.multiplicationservice.service.MultiplicationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = MultiplicationController.class)
@@ -29,59 +35,34 @@ class MultiplicationControllerTest {
     @MockBean
     private MultiplicationService service;
 
-    @Test
-    @DisplayName("Initial Test")
-    void initTest() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(10);
-        BigDecimal rightOpd = BigDecimal.valueOf(10);
-        Multiplication mockMultiply = new Multiplication(leftOpd, rightOpd, leftOpd.multiply(rightOpd), "Multiplication(*)");
-        Mockito.when(service.multiply(Mockito.any(), Mockito.any())).thenReturn(mockMultiply);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/multiply/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:10,rightOpd:10,answer:100,operation:Multiplication(*)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    static Stream<Arguments> inputs() {
+        return Stream.of(
+                arguments(10, 20),
+                arguments(-10, -20),
+                arguments(10, -20),
+                arguments(-10, 20),
+                arguments(10, 0)
+        );
     }
 
-    @Test
-    @DisplayName("if One Of The Value Is Negative Answer Should Be Negative")
-    void ifOneOfTheValueIsNegativeAnswerShouldBeNegative() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(10);
-        BigDecimal rightOpd = BigDecimal.valueOf(-10);
+    @ParameterizedTest
+    @DisplayName("All Test")
+    @MethodSource("inputs")
+    void initTest(int leftOp, int rightOp) throws Exception {
+        BigDecimal leftOpd = BigDecimal.valueOf(leftOp);
+        BigDecimal rightOpd = BigDecimal.valueOf(rightOp);
         Multiplication mockMultiply = new Multiplication(leftOpd, rightOpd, leftOpd.multiply(rightOpd), "Multiplication(*)");
         Mockito.when(service.multiply(Mockito.any(), Mockito.any())).thenReturn(mockMultiply);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/multiply/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/arithmetic/multiply/{leftOpd}/{rightOpd}", leftOpd, rightOpd))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.leftOpd", is(mockMultiply.getLeftOpd().intValue())))
+                .andExpect(jsonPath("$.rightOpd", is(mockMultiply.getRightOpd().intValue())))
+                .andExpect(jsonPath("$.answer", is(mockMultiply.getAnswer().intValue())))
+                .andExpect(jsonPath("$.operation", is(mockMultiply.getOperation())))
+                .andReturn();
         System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:10,rightOpd:-10,answer:-100,operation:Multiplication(*)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if Both Of The Values Are Negative Answer Should Be Positive")
-    void ifBothOfTheValuesAreNegativeAnswerShouldBePositive() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(-10);
-        BigDecimal rightOpd = BigDecimal.valueOf(-10);
-        Multiplication mockMultiply = new Multiplication(leftOpd, rightOpd, leftOpd.multiply(rightOpd), "Multiplication(*)");
-        Mockito.when(service.multiply(Mockito.any(), Mockito.any())).thenReturn(mockMultiply);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/multiply/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:-10,rightOpd:-10,answer:100,operation:Multiplication(*)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if One Of The Value Is Zero Answer Should Be Zero")
-    void ifOneOfTheValueIsZeroAnswerShouldBeZero() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(10);
-        BigDecimal rightOpd = BigDecimal.valueOf(0);
-        Multiplication mockMultiply = new Multiplication(leftOpd, rightOpd, leftOpd.multiply(rightOpd), "Multiplication(*)");
-        Mockito.when(service.multiply(Mockito.any(), Mockito.any())).thenReturn(mockMultiply);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/multiply/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:10,rightOpd:0,answer:0,operation:Multiplication(*)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
     }
 }
