c24634d7aca6dbc098ef6788ba231d745fb8f03e
Jay Rudani <jrudani4@gmail.com>
2023-02-08 17:59:28 +0530

    test updated to parameterized test

--- subtraction-service/src/test/java/com/arithmeticservices/subtractionservice/controller/SubtractionControllerTest.java
+++ subtraction-service/src/test/java/com/arithmeticservices/subtractionservice/controller/SubtractionControllerTest.java
@@ -2,9 +2,13 @@ package com.arithmeticservices.subtractionservice.controller;
 
 import com.arithmeticservices.subtractionservice.Subtraction;
 import com.arithmeticservices.subtractionservice.service.SubtractionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +20,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = SubtractionController.class)
@@ -29,87 +39,33 @@ class SubtractionControllerTest {
     @MockBean
     private SubtractionService service;
 
-    @Test
-    @DisplayName("Initial Test")
-    void initTest() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(5);
-        BigDecimal rightOpd = BigDecimal.valueOf(2);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:5,rightOpd:2,answer:3,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    static Stream<Arguments> inputs() {
+        return Stream.of(
+                arguments(10, 20),
+                arguments(-10, -20),
+                arguments(10, -20),
+                arguments(-10, 20)
+        );
     }
 
-    @Test
-    @DisplayName("if Both Values Are Negative And RightOpd Has Lower Value Then Answer Should Be Negative")
-    void ifBothValuesAreNegativeAndRightOpdHasLowerValueThenAnswerShouldBeNegative() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(-5);
-        BigDecimal rightOpd = BigDecimal.valueOf(-2);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    @ParameterizedTest
+    @DisplayName("All Test")
+    @MethodSource("inputs")
+    void initTest(int leftOp, int rightOp) throws Exception {
+        BigDecimal leftOpd = BigDecimal.valueOf(leftOp);
+        BigDecimal rightOpd = BigDecimal.valueOf(rightOp);
+        Subtraction subtraction = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
+        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(subtraction);
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.leftOpd", is(subtraction.getLeftOpd().intValue())))
+                .andExpect(jsonPath("$.rightOpd", is(subtraction.getRightOpd().intValue())))
+                .andExpect(jsonPath("$.answer", is(subtraction.getAnswer().intValue())))
+                .andExpect(jsonPath("$.operation", is(subtraction.getOperation())))
+                .andReturn();
         System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:-5,rightOpd:-2,answer:-3,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if Both Values Are Negative And LeftOpd Has Lower Value Then Answer Should Be Positive")
-    void ifBothValuesAreNegativeAndLeftOpdHasLowerValueThenAnswerShouldBePositive() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(-2);
-        BigDecimal rightOpd = BigDecimal.valueOf(-5);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:-2,rightOpd:-5,answer:3,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if LeftOpd Is Negative And LeftOpd Has Lower Value Then Answer Should Be Negative")
-    void ifLeftOpdIsNegativeAndLeftOpdHasLowerValueThenAnswerShouldBeNegative() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(-2);
-        BigDecimal rightOpd = BigDecimal.valueOf(5);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:-2,rightOpd:5,answer:-7,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if RightOpd Is Negative Then Answer Should Be Positive")
-    void ifRightOpdIsNegativeThenAnswerShouldBePositive() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(2);
-        BigDecimal rightOpd = BigDecimal.valueOf(-5);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:2,rightOpd:-5,answer:7,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
-    }
-
-    @Test
-    @DisplayName("if Both Values Are Positive And RightOpd Has Higher Value Then Answer Should Be Negative")
-    void ifBothValuesArePositiveAndRightOpdHasHigherValueThenAnswerShouldBeNegative() throws Exception {
-        BigDecimal leftOpd = BigDecimal.valueOf(2);
-        BigDecimal rightOpd = BigDecimal.valueOf(6);
-        Subtraction mockSubtract = new Subtraction(leftOpd, rightOpd, leftOpd.subtract(rightOpd), "Subtraction(-)");
-        Mockito.when(service.subtract(Mockito.any(), Mockito.any())).thenReturn(mockSubtract);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/arithmetic/subtraction/{leftOpd}/{rightOpd}", leftOpd, rightOpd).accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
-        String expected = "{leftOpd:2,rightOpd:6,answer:-4,operation:Subtraction(-)}";
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
     }
 }
